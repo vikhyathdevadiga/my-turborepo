b33ece9f232483481f532b540fd3a836b2a0b273
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-03-24 13:56:12 +0530

    Update eslint.config.js

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
 import { nextJsConfig } from "@repo/eslint-config/next-js";
-
+console.log(process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY)
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
