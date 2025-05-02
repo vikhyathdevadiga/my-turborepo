d0b48268075ddb38b08fad89794fe7f8617c446a
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-05-02 10:47:04 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-
+export const dynamic = "force-dynamic" 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
   srcDark: string;
